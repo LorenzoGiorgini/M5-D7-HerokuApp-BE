788434f7425b056a266a9a194946ea022d82bbe4
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-10-27 15:51:33 +0200

    Update pdf.js

--- src/tools/pdf.js
+++ src/tools/pdf.js
@@ -1,19 +1,5 @@
 import PdfPrinter from "pdfmake";
 
-//CONVERTS THE IMAGE IN base64 format
-function dataURLtoFile(dataurl, filename) {
-    var arr = dataurl.split(","),
-      mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]),
-      n = bstr.length,
-      u8arr = new Uint8Array(n);
-  
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-  
-    return new File([u8arr], filename, { type: mime });
-}
 
 export const createPDF = (data) => {
     let fonts = {
@@ -27,17 +13,13 @@ export const createPDF = (data) => {
 
 
     if(data.imageUrl !== ""){
-
-        const base64 = data.imageUrl.toDataURL();
-
-        const file = dataURLtoFile(base64, "image.png");
     
         let docDefinition = {
             content: [
-                {
+                /* {
                     image: file,
                     width: "100%"
-                },
+                } */,
                 {
                     text: `${data.name}`,
                     style: "header"
